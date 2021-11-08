4bd2d992a89ea56192bd3e5aca7f10ce87136491
mohamedelserafy <elserafy02@gmail.com>
2021-11-08 18:30:19 +0200

    email service

---
+++ services/email.js
@@ -0,0 +1,42 @@
+const mailer = require("nodemailer");
+const nodemailer = require("nodemailer");
+
+const sendVerificationCode = async (email, token) => {
+  let transporter;
+  if (process.env.NODE_ENV !== "production") {
+    let testAccount = await nodemailer.createTestAccount();
+
+    transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIl_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo" <bosta@example.com>' || process.env.EMAIl_ADDRESS, // sender address
+    to: email,
+    subject: "email verification",
+    text: `Hey there, here's your token ${token}`,
+    html: `<b>Hey there, here's your token ${token} </b>`,
+  });
+  if (process.env.NODE_ENV !== "production") {
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  }
+};
+
+module.exports = { sendVerificationCode };
