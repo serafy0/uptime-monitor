c6552acda757a4f9fd5a0237aa3d4823446760d9
mohamedelserafy <elserafy02@gmail.com>
2021-11-15 10:36:21 +0200

    report model

---
+++ models/report.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const reportSchema = new mongoose.Schema({
+  status: {},
+  availability: Number,
+  outages: { type: Number, default: 0 },
+  downtimeInSeconds: { type: Number, default: 0 },
+  uptimeInSeconds: { type: Number, default: 0 },
+  responseTime: { type: Number },
+  history: [{ type: mongoose.Schema.Types.ObjectId, ref: "check" }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+});
+
+module.exports = Report = mongoose.model("report", reportSchema);
