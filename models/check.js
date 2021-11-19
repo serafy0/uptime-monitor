c92123b6cbc71db56867ac0cc096993f0b80794e
mohamedelserafy <elserafy02@gmail.com>
2021-11-20 00:15:35 +0200

    update check model

--- models/check.js
+++ models/check.js
@@ -8,15 +8,19 @@ const checkSchema = new mongoose.Schema({
     lowercase: true,
     trim: true,
   },
+  path: {
+    type: String,
+    lowercase: true,
+    trim: true,
+  },
   protocol: {
     type: String,
-    enum: ["http", "https", "tcp"],
+    enum: ["http:", "https:", "tcp"],
     required: true,
   },
   port: Number,
   webhook: {
     type: String,
-    lowercase: true,
   },
   timeoutInSeconds: { type: Number, default: 5 },
   interval: { type: Number, default: 10 * 60 },
@@ -25,16 +29,13 @@ const checkSchema = new mongoose.Schema({
     username: String,
     password: String,
   },
-  httpHeaders: {
-    type: Map,
-    of: String,
-    default: {},
-  },
+  httpHeaders: mongoose.Schema.Types.Mixed,
   assert: {
     statusCode: { type: Number, max: 599, min: 100 },
   },
   tags: [{ type: String }],
   ignoreSSL: { type: Boolean, default: true },
+  requests: [{ type: mongoose.Schema.Types.ObjectId, ref: "request" }],
   creator: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
 });
 
