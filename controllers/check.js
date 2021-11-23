0d99878a12effc30b7f4a98d096657ca00bbc539
mohamedelserafy <elserafy02@gmail.com>
2021-11-23 04:25:00 +0200

    check controller

---
+++ controllers/check.js
@@ -0,0 +1,58 @@
+const Check = require("../models/check");
+const { removeJob, addCheckJob } = require("../queues/check");
+
+exports.addCheck = async (req, res, next) => {
+  try {
+    const {
+      name,
+      linkText,
+      webhook,
+      timeout,
+      interval,
+      threshold,
+      authentication,
+      httpHeaders,
+      assertStatus,
+      ignoreSSL,
+    } = req.body;
+    const newURL = new URL(linkText);
+    console.log(newURL);
+    const newCheck = await Check.create({
+      name: name,
+      url: newURL.origin,
+      path: newURL.pathname,
+      protocol: newURL.protocol,
+      port: newURL.port,
+      webhook: webhook,
+      timeoutInSeconds: timeout,
+      interval: interval,
+      assert: { statusCode: assertStatus },
+      ignoreSSL: ignoreSSL,
+      authentication: authentication,
+      httpHeaders: httpHeaders,
+      threshold: threshold,
+    });
+
+    await addCheckJob(newCheck);
+
+    return res.status(201).json({
+      newCheck: newCheck,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.pauseCheck = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const pausedCheck = await Check.findById(id);
+    if (!pausedCheck) {
+      return res.status(404).json({ error: "check not found" });
+    }
+    await removeJob(pausedCheck);
+
+    return res.status(200).json({ message: "check paused" });
+  } catch (err) {
+    next(err);
+  }
+};
