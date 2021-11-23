1ac4c068b379640d9329446760b38e366d9f40a3
mohamedelserafy <elserafy02@gmail.com>
2021-11-23 04:23:18 +0200

    update: http error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,19 @@
 const apiError = require("../utils/apiError");
+const { HTTPError, TimeoutError } = require("got");
 
 function errorHandler(err, req, res, next) {
   if (err instanceof apiError) {
     return res.status(err.status).json({ error: err.message });
+  }
+
+  if (err instanceof HTTPError) {
+    return res.status(200).json({ message: err.response.statusCode });
+  }
+  if (err instanceof TimeoutError) {
+    const responseTimeInSeconds = err.timings.phases.total / 1000;
+    return res
+      .status(200)
+      .json({ error: err, responseTimeInSeconds: responseTimeInSeconds });
   }
   return res
     .status(err.status || 500)
