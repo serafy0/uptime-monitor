9161f974f6e4fc91bceb9699871eb40d05af96ad
mohamedelserafy <elserafy02@gmail.com>
2021-11-23 04:28:08 +0200

    add request

---
+++ services/request.js
@@ -0,0 +1,56 @@
+const Request = require("../models/request");
+const Check = require("../models/check");
+const got = require("got");
+
+exports.addRequest = async ({ status, responseDuration, error }, check) => {
+  try {
+    const request = await Request.create({
+      status: status,
+      responseDuration: responseDuration,
+      error: error,
+      check: check._id,
+    });
+
+    let isDown = check.isDown; //true
+    if (check.webhook) {
+      if (check.isDown && (!error || check.assert.statusCode === status)) {
+        isDown = false;
+        await sendToWebhook(
+          check.webhook,
+          `${check.url + check.path} is working as expected`
+        );
+      }
+
+      if (!check.isDown && error && check.assert.statusCode !== status) {
+        isDown = true;
+        await sendToWebhook(
+          check.webhook,
+          `${check.url + check.path} is down ${error}`
+        );
+      }
+    }
+
+    const updatedCheck = await Check.findOneAndUpdate(
+      { _id: check._id },
+      { isDown: isDown, $push: { requests: request._id } }
+    );
+  } catch (err) {
+    return;
+  }
+};
+
+const sendToWebhook = async (webhook, message) => {
+  try {
+    if (webhook) {
+      await got({
+        url: webhook,
+        method: "POST",
+        body: JSON.stringify({
+          text: message,
+        }),
+      });
+    }
+  } catch (err) {
+    return;
+  }
+};
