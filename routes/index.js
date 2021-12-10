51c4615b3c67a2c43fd44f901cdefc13ed49ac6a
mohamedelserafy <elserafy02@gmail.com>
2021-12-11 01:52:47 +0200

    swagger docs

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ const router = Router();
 const authRouter = require("./auth");
 const checkRouter = require("./check");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
+
 router.use("/auth", authRouter);
 router.use("/check", checkRouter);
 
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerDocument));
 router.get("*", (req, res) => {
   res.status(404).json({ error: "Not Found" });
 });
